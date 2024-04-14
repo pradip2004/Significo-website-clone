83ba82406ac58d55aecda4f1c446a426a83d4a2d
pradip2004 <pradipsadhukhan16@gmail.com>
2024-04-14 10:26:58 +0530

    teamMemberSection Animation added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const homeAnimation = ()=>{
+const homeAnimation = () => {
       gsap.set(".animated-slide-heading", {
             scale: 3
       })
@@ -11,7 +11,7 @@ const homeAnimation = ()=>{
             },
       })
       const t2 = gsap.timeline({
-            scrollTrigger:{
+            scrollTrigger: {
                   trigger: ".home-section",
                   scrub: 1,
                   start: "60% 50%",
@@ -19,25 +19,25 @@ const homeAnimation = ()=>{
                   // markers: true
             }
       })
-      t1.to(".home-section-video",{
+      t1.to(".home-section-video", {
             '--clip': "0%",
             ease: Power2
       }, "a")
-      t1.to(".animated-slide-heading",{
+      t1.to(".animated-slide-heading", {
             scale: 1
       }, "a")
       t2.to(".text-rows-left", {
             xPercent: -20,
             stagger: 0.05,
-            ease:  Power4
+            ease: Power4
       }, "b")
       t2.to(".text-rows:nth-child(2)", {
             xPercent: 10,
             stagger: 0.1,
-            ease:  Power4
+            ease: Power4
       }, "b")
 }
-const partnerSectionAnimation = ()=>{
+const partnerSectionAnimation = () => {
       gsap.to(".slide", {
             scrollTrigger: {
                   trigger: ".partner-section-container",
@@ -47,12 +47,34 @@ const partnerSectionAnimation = ()=>{
                   scrub: 2
             },
             xPercent: -300,
-            ease:  Power4
+            ease: Power4
       })
 }
+const teamAnimation = () => {
+      document.querySelectorAll(".team-member")
+            .forEach(function (member) {
+                  member.addEventListener('mousemove', function (e) {
+                        gsap.to(this.querySelector(".hover-picture"), {
+                              opacity: 1,
+                              x: gsap.utils.mapRange(0, window.innerWidth, -200, 200, e.clientX),
+                              ease: Power4,
+                              duration: .5
+                        })
 
+                  })
+                  member.addEventListener('mouseleave', function (e) {
+                        gsap.to(this.querySelector(".hover-picture"), {
+                              opacity: 0,
+                              ease: Power4,
+                              duration: .5
+                        })
+
+                  })
+            })
+}
 
 
 
 homeAnimation();
 partnerSectionAnimation();
+teamAnimation()
